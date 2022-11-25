9c638014d65a8c097c0566662865ebafab1aec06
jjzapata99 <jjzapata@espol.edu.ec>
2022-11-25 18:39:02 -0500

    Avance trivia x2

--- app/src/main/java/com/example/trivicini/game_p.java
+++ app/src/main/java/com/example/trivicini/game_p.java
@@ -3,9 +3,12 @@ package com.example.trivicini;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class game_p extends AppCompatActivity {
     Button btn1, btn2, btn3, btn4;
@@ -22,20 +25,36 @@ public class game_p extends AppCompatActivity {
             {"Rayo McQueen", "Sally", "Doc Hudson", "Guido"},
             {"Sylvester Stallone", "Eugenio Derbez", "Miguel Elizalde", "Morgan Freeman"},
             {"James Cameron", "Steven Spielberg", "Martin Scorsese", "Quentin Tarantino"}};
-
-
+    int[] image = {R.drawable.trivia1,R.drawable.trivia2,R.drawable.trivia3,R.drawable.trivia4};
+    Random rand = new Random();
     int contador=0;
-
+    int tried =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_p);
         txt_trivia=findViewById(R.id.txt_trivia);
-        img_trivia=findViewById(R.id.img_trivia);
+        img_trivia=(ImageView) findViewById(R.id.img_trivia);
         btn1= (Button) findViewById(R.id.btn_trivia1);
         btn2= (Button) findViewById(R.id.btn_trivia2);
         btn3= (Button) findViewById(R.id.btn_trivia3);
         btn4= (Button) findViewById(R.id.btn_trivia4);
 
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                img_trivia.setImageResource(image[tried]);
+                txt_trivia.setText(trivia[tried]);
+
+                btn1.setText(respuesta[tried][0]);
+            }
+        });
+    }
+    public int[] generateRandom(int n){
+        int arreglo[]= new int[n];
+        for(int i=0; i<=n; i++) {
+            arreglo[i] = (int) (rand.nextInt(4) + 1);
+        }
+        return arreglo;
     }
 }
