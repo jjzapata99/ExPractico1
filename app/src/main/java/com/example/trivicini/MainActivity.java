b00793a9b31224b8711f164197ea608afc948d07
jjzapata99 <jjzapata@espol.edu.ec>
2022-11-25 15:45:01 -0500

    game View

--- app/src/main/java/com/example/trivicini/MainActivity.java
+++ app/src/main/java/com/example/trivicini/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.trivicini;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_login = (Button)findViewById(R.id.btn_empezar);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this, game_p.class);
+                startActivity(intent);
+                 } });
     }
 }
