aa5f65abeba766cfb6e850274595ae74d17ecf2e
jjzapata99 <jjzapata@espol.edu.ec>
2022-11-26 10:21:51 -0500

    Avance, corregir valores random sin repetir

--- app/src/main/java/com/example/trivicini/game_p.java
+++ app/src/main/java/com/example/trivicini/game_p.java
@@ -2,12 +2,14 @@ package com.example.trivicini;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.Console;
 import java.util.Random;
 
 public class game_p extends AppCompatActivity {
@@ -25,10 +27,12 @@ public class game_p extends AppCompatActivity {
             {"Rayo McQueen", "Sally", "Doc Hudson", "Guido"},
             {"Sylvester Stallone", "Eugenio Derbez", "Miguel Elizalde", "Morgan Freeman"},
             {"James Cameron", "Steven Spielberg", "Martin Scorsese", "Quentin Tarantino"}};
-    int[] image = {R.drawable.trivia1,R.drawable.trivia2,R.drawable.trivia3,R.drawable.trivia4};
+    int[] image = {R.drawable.trivia1,R.drawable.trivia2,R.drawable.trivia3,R.drawable.trivia4,R.drawable.trivia5};
     Random rand = new Random();
     int contador=0;
     int tried =0;
+    int[] value_rand = generateRandom(5);
+    int [] value_rand_button = generateRandom(4);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,22 +43,67 @@ public class game_p extends AppCompatActivity {
         btn2= (Button) findViewById(R.id.btn_trivia2);
         btn3= (Button) findViewById(R.id.btn_trivia3);
         btn4= (Button) findViewById(R.id.btn_trivia4);
-
+        change(tried);
+        tried++;
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                img_trivia.setImageResource(image[tried]);
-                txt_trivia.setText(trivia[tried]);
-
-                btn1.setText(respuesta[tried][0]);
+                    change(tried);
+                    tried++;
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                change(tried);
+                tried++;
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                change(tried);
+                tried++;
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                change(tried);
+                tried++;
             }
         });
     }
     public int[] generateRandom(int n){
         int arreglo[]= new int[n];
-        for(int i=0; i<=n; i++) {
-            arreglo[i] = (int) (rand.nextInt(4) + 1);
+        for(int i=0; i<n; i++) {
+            int num_rand = (int) (rand.nextInt(n) );
+            for (int x = 0; x <= i; x++) {
+                if (arreglo[x] != num_rand) {
+                    arreglo[i] = num_rand;
+                }
+            }
         }
         return arreglo;
+    }
+    public void change(int verif) {
+        if (verif < 5) {
+            if(contador <3) {
+                img_trivia.setImageResource(image[value_rand[verif]]);
+                txt_trivia.setText(trivia[value_rand[verif]]);
+
+                btn1.setText(respuesta[value_rand[verif]][value_rand_button[0]]);
+                btn2.setText(respuesta[value_rand[verif]][value_rand_button[1]]);
+                btn3.setText(respuesta[value_rand[verif]][value_rand_button[2]]);
+                btn4.setText(respuesta[value_rand[verif]][value_rand_button[3]]);
+            }
+            else {
+                Intent intent= new Intent(game_p.this, activity_lose.class);
+                startActivity(intent);
+            }
+        } else {
+            Intent intent= new Intent(game_p.this, activity_lose.class);
+            startActivity(intent);
+        }
     }
 }
