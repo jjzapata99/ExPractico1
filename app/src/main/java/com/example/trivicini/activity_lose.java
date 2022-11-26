85bdfa5eb1be4ac712346144ea14899a5de554f2
JeffersonCabello <jefcsanc@espol.edu.ec>
2022-11-26 09:49:28 -0500

    Correcion de lose_activity

--- app/src/main/java/com/example/trivicini/activity_lose.java
+++ app/src/main/java/com/example/trivicini/activity_lose.java
@@ -2,13 +2,38 @@ package com.example.trivicini;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity_lose extends AppCompatActivity {
+
+    private Button btnJugar;
+    private Button btnSalir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lose);
+
+        btnJugar = (Button) findViewById(R.id.btn_volver_a_jugar);
+        btnSalir = (Button) findViewById(R.id.btn_salir);
+
+        btnJugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(activity_lose.this, game_p.class);
+                startActivity(intent);
+            }
+        });
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(activity_lose.this, MainActivity.class);
+                startActivity(intent2);
+            }
+        });
+
     }
 }
