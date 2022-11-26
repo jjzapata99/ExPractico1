f45a02c83fa7d992f6f2cc3fbf49e4e6bc2e3524
JeffersonCabello <jefcsanc@espol.edu.ec>
2022-11-25 20:09:10 -0500

    pantalla de perdida

---
+++ app/src/main/java/com/example/trivicini/activity_lose.java
@@ -0,0 +1,14 @@
+package com.example.trivicini;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_lose extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lose);
+    }
+}
