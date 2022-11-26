18335de827d9df41943fa97aeea4e54a9ce72084
JeffersonCabello <jefcsanc@espol.edu.ec>
2022-11-25 21:34:33 -0500

    EventosBotones

--- app/src/main/java/com/example/trivicini/activity_lose.java
+++ app/src/main/java/com/example/trivicini/activity_lose.java
@@ -2,13 +2,35 @@ package com.example.trivicini;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity_lose extends AppCompatActivity {
+
+    Button btnJugar = findViewById(R.id.btn_volver_a_jugar);
+    Button btnSalir = findViewById(R.id.btn_salir);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lose);
+
+        btnJugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(activity_lose.this, game_p.class);
+                startActivity(intent);
+            }
+        });
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(activity_lose.this, MainActivity.class);
+                startActivity(intent2);
+            }
+        });
+
     }
 }
